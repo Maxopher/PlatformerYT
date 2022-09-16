417fe915aef12a3174cd86fb89910bafbae0803d
Maxopher <75018398+Maxopher@users.noreply.github.com>
2022-09-16 23:31:28 +0200

    Restarted and got everything working again hopefully

--- player.py
+++ player.py
@@ -1,18 +1,40 @@
 import pygame
+from support import import_folder
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        self.image = pygame.Surface((32, 64))
-        self.image.fill('red')
+        self.animations = {'idle': [], 'run': [], 'jump': []}
+        self.import_character_assets()
+        self.frame_index = 0
+        self.animation_speed = 0.15
+        self.image = pygame.image.load('graphics/character/idle/2.png')
+        # self.image = self.animations['idle'][self.frame_index]
         self.rect = self.image.get_rect(topleft=pos)
-
 
         # player movement
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 10
         self.gravity = 0.8
         self.jump_speed = -16
+
+    def import_character_assets(self):
+        character_path = 'graphics/character/'
+        self.animations = {'idle': [], 'run': [], 'jump': []}
+        for animation in self.animations.keys():
+            full_path = character_path + animation
+            self.animations[animation] = import_folder(full_path)
+
+    def animate(self):
+        animation = self.animations['run']
+
+        # loop over frame index
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        self.image = animation[int(self.frame_index)]
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -36,3 +58,4 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.get_input()
+        self.animate()
---
+++ support.py
@@ -0,0 +1,12 @@
+from os import listdir
+import pygame
+
+def import_folder(path):
+    surface_list = []
+
+    for image in listdir(path):
+        full_path = path + '/' + image
+        image_surf = pygame.image.load(full_path).convert_alpha()
+        surface_list.append(image_surf)
+
+    return surface_list
