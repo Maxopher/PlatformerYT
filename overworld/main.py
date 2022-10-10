d8d740bd6bc851035f0eb0ef4f317136597fc758
Maxopher <75018398+Maxopher@users.noreply.github.com>
2022-10-10 22:20:49 +0200

    Set up main files and overworld logic

--- overworld/main.py
+++ overworld/main.py
@@ -1,14 +1,23 @@
 import pygame
 import sys
 from settings import *
-from level import Level
+from overworld import Overworld
+
+
+class Game:
+    def __init__(self):
+        self.max_level = 3
+        self.overworld = Overworld(0, self.max_level, screen)
+
+    def run(self):
+        self.overworld.run()
 
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
-level = Level(level_map, screen)
+game = Game()
 
 while True:
     for event in pygame.event.get():
@@ -17,7 +26,7 @@ while True:
             sys.exit()
 
     screen.fill('black')
-    level.run()
+    game.run()
 
     pygame.display.update()
     clock.tick(60)
